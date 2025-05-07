cfb7b91833b48032233e5d2e0a86436516768b94
tritonro <106782621+tritonro@users.noreply.github.com>
2025-05-06 22:18:07 -0700

    Added voice list options from speechSynthesis to options in selection menu

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,32 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  //populate Voice List: code adapted closely from MDN Speech Synthesis docs 
+
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+  
+    const voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+
+speechSynthesis.addEventListener("voiceschanged", function (event){
+  populateVoiceList(); 
+})
+  
 }
